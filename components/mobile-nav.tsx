2c0a7221b0fb5d1555e62c5658ca108f665acbc3
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-02-27 19:14:22 +0200

    update: fixed z-index issure on nav

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import { X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
 
 interface MobileNavProps {
   isOpen: boolean;
@@ -18,7 +19,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ isOpen, setIsOpen }) => {
           animate={{ y: "0%", opacity: 1 }} // Slide into place
           exit={{ y: "-100%", opacity: 0 }} // Slide out when closed
           transition={{ duration: 0.5, ease: "easeInOut" }}
-          className="fixed inset-0 bg-black dark:bg-white text-white dark:text-black h-[30%] shadow-lg"
+          className="fixed inset-0 bg-black dark:bg-white text-white dark:text-black h-[30%] shadow-lg z-50"
         >
           <X
             className="text-white dark:text-black h-10 w-10 ml-auto mt-4 mr-4 cursor-pointer"
@@ -27,13 +28,13 @@ const MobileNav: React.FC<MobileNavProps> = ({ isOpen, setIsOpen }) => {
 
           <ul className="font-semibold space-y-4 text-lg ml-4 mt-8">
             <li>
-              <a href="/">Home</a>
+              <Link href="/">Home</Link>
             </li>
             <li>
-              <a href="/#projects">Projects</a>
+              <Link href="/#projects">Projects</Link>
             </li>
             <li>
-              <a href="mailto:bekithemba.mdluli.bm@gmail.com">Contact</a>
+              <Link href="mailto:bekithemba.mdluli.bm@gmail.com">Contact</Link>
             </li>
           </ul>
         </motion.div>
