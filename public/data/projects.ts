f03a90376de340a40159552a84777869389f10aa
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-03-16 22:04:51 +0200

    update: added wealth sync to projects

--- public/data/projects.ts
+++ public/data/projects.ts
@@ -1,5 +1,14 @@
 const projects = [
   {
+    id: 3,
+    title: "Wealth Sync",
+    text: "WealthSync is a personal finance management application designed to help users track their budgets, savings, and stock investments. It features a secure backend built with ASP.NET Core, a responsive Angular frontend, and integrates with Financial Modeling Prep (FMP) for real-time stock data, including adjusted dividend yields. The app supports user authentication with JWT, budget management, and stock portfolio tracking with a limit of 3 stocks per user.",
+    repo: "https://github.com/BMdluli/WealthSync.App/",
+    live: "https://wealth-sync-wine.vercel.app",
+    second: "",
+    image: "/wealth-sync.png",
+  },
+  {
     id: 0,
     title: "Raspberry: Track Savings",
     text: "This is an ios app that helps users create savings goals and track progress of savings.",
